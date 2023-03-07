c57c482368bfa4b1515da37fa01a4f6f3128d453
FrancoFerreira19956 <frankocucsi@gmail.com>
2023-03-07 19:49:51 -0300

    proyecto final

---
+++ jav.js
@@ -0,0 +1,17 @@
+document.getElementById('boton').onclick = function () {
+    console.log("rfgsgf");
+    document.getElementById("parrafo").innerHTML = "TommyAlvarez@ejemplo.com";
+
+}
+
+document.getElementById('boton2').onclick = function () {
+console.log("rfgsgf");
+document.getElementById("parrafo").innerHTML = "(341) 795-4320";
+
+}
+
+document.getElementById('boton3').onclick = function () {
+console.log("rfgsgf");
+document.getElementById("parrafo").innerHTML = "5405 Stevens Creek Blvd";
+
+}
